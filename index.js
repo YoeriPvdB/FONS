5958c256349f972f7b67b4c37d3a8d80a3707676
Yoeri Pearcey van der Beek <37370340+EdBanger0123@users.noreply.github.com>
2022-04-24 19:26:22 +0100

    added port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 var express = require("express");
-var port = 8000;
+var port = app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });;
 var app = express();
 
 
